c332fd6d9d4a7e00946439ce431a0d03a4152a25
victoricoma <victorsena3010@gmail.com>
2021-12-22 14:47:07 -0300

    Implementação do módulo de validação de rotas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import controlerUserLogin from "./moduloInterface.js";
+import controlerUserLogin from "./moduloControlerLogin.js";
+import controleRotasApp from "./moduloControleRotas.js";
+
 //Trazendo elementos do Bootstrap para uso da main Javascript.
 var myModal = new bootstrap.Modal(document.getElementById('myModal'))
-//Trazendo os botões da DOM em HTML.
+//Ordem para executar e validar o Login.
 const entrarUsuario = document.getElementById("btnEntrar");
 entrarUsuario.addEventListener("click", (event)=>{
     let user = document.getElementById("usuario").value,
@@ -14,4 +16,13 @@ entrarUsuario.addEventListener("click", (event)=>{
     document.getElementById("btnModalSave").innerHTML=alertModal.b2;
 
     myModal.show();
+    setTimeout(carregarPagina, 5000);
+    function carregarPagina(){
+        window.location.href=controleRotasApp.validaRota(localStorage.status,alertModal.idModal);
+    }
+});
+//Ordem para criar um novo usuário.
+const novoUsuario = document.getElementById("btnNovoUsuario");
+novoUsuario.addEventListener("click", (event) =>{
+    window.location.href=controleRotasApp.validaRota("false", "usuarioNaoExiste1");
 });
