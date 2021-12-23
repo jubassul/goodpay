f026ae2990a892191f367b314001e6cd7b57e419
DianadosSantos <dianasilvadossantos228@gmail.com>
2021-12-23 16:45:45 -0300

    Inserção de Dados de Erros

--- src/moduloAjudaDados.js
+++ src/moduloAjudaDados.js
@@ -4,15 +4,23 @@ const ajudaDados = [
         idTitle: "gp-error#101",
         title: "Usuario não encontrado ou inválido",
         desc: "Os nomes de usuários devem ser compostos por letras e números, sem espaços ou caracteres especiais.",
-        msg: "Caso o usuário não esteja cadastrado, cadastre-o novamente.",
+        msg: "Caso o usuário não esteja cadastrado, cadastre-se",
     },
     {
         id: "102",
         idTitle: "gp-error#102",
         title: "Senha inválida",
-        desc: "A senha deve conter no mínimo 6 caracteres e não deve possuir caracteres especiais.",
-        msg: "Caso o usuário não esteja cadastrado, cadastre-o novamente.",
+        desc: "A senha deve conter no mínimo 6 caracteres, no mínimo uma letra maiúscula e uma minúscula e não deve possuir caracteres especiais.",
+        msg: "Caso o erro permaneça, redefina sua senha aqui",
     },
+    {
+        id:"103",
+        idTitle: "gp-error#103",
+        title:"Campo Vazio",
+        desc:"Seu usuário ou sua senha estão vazios",
+        msg:"O campo de usuário e senha precisam estar preenchidos",
+    },
+    
 ];
 
 export default ajudaDados;
